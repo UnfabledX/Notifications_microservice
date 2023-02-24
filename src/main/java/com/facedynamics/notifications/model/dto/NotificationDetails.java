4b9b4dc6ac11d5bf170bc76acffa429dd3546adf
Oleksii Synelnyk <osynelnyk@griddynamics.com>
2023-02-24 15:40:31 +0200

    implement sending email, tests

--- src/main/java/com/facedynamics/notifications/model/dto/NotificationDetails.java
+++ src/main/java/com/facedynamics/notifications/model/dto/NotificationDetails.java
@@ -2,9 +2,12 @@ package com.facedynamics.notifications.model.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 @Data
@@ -12,11 +15,14 @@ import java.time.LocalDateTime;
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY,
              content = JsonInclude.Include.NON_NULL)
 public class NotificationDetails {
-
+    @Positive
     private int userId;
+    @Size(min = 3, max = 24, message =  "Username mustn't be bigger then 24 letters and less then 3 letters")
     private String userName;
+    @Email(message = "Email is written in a wrong format")
     private String userEmail;
     private String link;
+    @Positive
     private int timeToLive;
     private String commentText;
     private String replyText;
--- src/main/java/com/facedynamics/notifications/model/dto/NotificationGetDTO.java
+++ src/main/java/com/facedynamics/notifications/model/dto/NotificationGetDTO.java
@@ -3,16 +3,22 @@ package com.facedynamics.notifications.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 
 @Data
 @AllArgsConstructor
 public class NotificationGetDTO {
 
     @NotEmpty
+    @Positive
     private int ownerId;
+
     @NotEmpty
     private String notificationType;
+
+    @Valid
     @NotEmpty
     private NotificationDetails details;
 }
