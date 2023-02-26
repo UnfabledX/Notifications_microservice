59606e9e99b843e3d8a413bb68cd103b9d336a0a
Oleksii Synelnyk <osynelnyk@griddynamics.com>
2023-02-26 12:01:36 +0200

    split createNotification method, change some annotations

--- src/main/java/com/facedynamics/notifications/model/dto/NotificationDetails.java
+++ src/main/java/com/facedynamics/notifications/model/dto/NotificationDetails.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY,
              content = JsonInclude.Include.NON_NULL)
 public class NotificationDetails {
-    @Positive
+    @Min(1)
     private int userId;
     @Size(min = 3, max = 24, message =  "Username mustn't be bigger then 24 letters and less then 3 letters")
     private String userName;
