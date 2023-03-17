4859ed2cd30ba6694ba9bd95c8cd316cb304ca43
Oleksii Synelnyk <osynelnyk@griddynamics.com>
2023-03-17 09:57:08 +0200

    add some tests

--- src/test/java/com/facedynamics/notifications/IntegrationTests.java
+++ src/test/java/com/facedynamics/notifications/IntegrationTests.java
@@ -3,9 +3,7 @@ package com.facedynamics.notifications;
 import com.facedynamics.BaseTest;
 import com.facedynamics.notifications.handler.Error;
 import com.facedynamics.notifications.model.NotificationResponseDTO;
-import com.facedynamics.notifications.model.dto.NotificationContent;
-import com.facedynamics.notifications.model.dto.NotificationDto;
-import com.facedynamics.notifications.model.dto.PostCommented;
+import com.facedynamics.notifications.model.dto.*;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +21,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static com.facedynamics.notifications.model.dto.NotificationContent.Type.POST_COMMENTED;
+import static com.facedynamics.notifications.model.dto.NotificationContent.Type.*;
+import static com.facedynamics.notifications.utils.Constants.GREATER_THAN_OR_EQUAL_TO_1;
 import static com.facedynamics.notifications.utils.SqlStatements.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -39,7 +38,7 @@ public class IntegrationTests extends BaseTest {
     @LocalServerPort
     private int port;
 
-    private int userId;
+    private Long userId;
 
     private String createURLWithPort() {
         return "http://localhost:" + port + "/notifications";
@@ -48,7 +47,7 @@ public class IntegrationTests extends BaseTest {
     @Test
     @Order(1)
     public void getAllNotificationsByUserIdTest_validId() {
-        userId = 4;
+        userId = 4L;
         int pageNumber = 0;
         ResponseEntity<Map> response = template.getForEntity(
                 createURLWithPort() + "/users/{userId}?page={pageNumber}",
@@ -62,7 +61,7 @@ public class IntegrationTests extends BaseTest {
     @Test
     @Order(2)
     public void getAllNotificationsByUserIdTest_notValidId() {
-        userId = 4321;
+        userId = 4321L;
         int pageNumber = 0;
         ResponseEntity<Error> response = template.exchange(
                 createURLWithPort() + "/users/{userId}?page={pageNumber}", HttpMethod.GET,
@@ -73,7 +72,7 @@ public class IntegrationTests extends BaseTest {
     @Test
     @Order(2)
     public void getAllNotificationsByUserIdTest_notValidInput() {
-        userId = 4;
+        userId = 4L;
         ResponseEntity<Map> response = template.exchange(
                 createURLWithPort() + "/users/{userId}?page={pageNumber}", HttpMethod.GET,
                 null, Map.class, userId, "abc");
@@ -87,7 +86,7 @@ public class IntegrationTests extends BaseTest {
     @Order(4)
     @Sql(statements = {details1, notific1}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void deleteAllNotificationsByUserIdTest_validId() {
-        userId = 10;
+        userId = 10L;
         ResponseEntity<String> response = template.exchange(createURLWithPort() + "/users/{userId}",
                 HttpMethod.DELETE, null, String.class, userId);
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -96,7 +95,7 @@ public class IntegrationTests extends BaseTest {
     @Test
     @Order(5)
     public void deleteAllNotificationsByUserIdTest_IdIsNotPresent() {
-        userId = 432100;
+        userId = 432100L;
         ResponseEntity<Error> response = template.exchange(createURLWithPort() + "/users/{userId}",
                 HttpMethod.DELETE, null, Error.class, userId);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
@@ -104,6 +103,25 @@ public class IntegrationTests extends BaseTest {
 
     @Test
     @Order(6)
+    public void deleteAllNotificationsByUserIdTest_IdIsNegative() {
+        userId = -1L;
+        ResponseEntity<ProblemDetail> response = template.exchange(createURLWithPort() + "/users/{userId}",
+                HttpMethod.DELETE, null, ProblemDetail.class, userId);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+        ProblemDetail details = response.getBody();
+        assertNotNull(details);
+        Map<String, Object> map = details.getProperties();
+        assertNotNull(map);
+
+        ArrayList<?> list = (ArrayList<?>) map.get("problemDetails");
+        LinkedHashMap<?,?> innerData = (LinkedHashMap<?,?>) list.get(0);
+        String actual = innerData.get("message").toString();
+        assertEquals(GREATER_THAN_OR_EQUAL_TO_1, actual);
+    }
+
+    @Test
+    @Order(7)
     @Sql(statements = {details2, notific2}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void deleteNotificationByIdTest_idIsPresent() {
         long notificationId = 11;
@@ -114,16 +132,36 @@ public class IntegrationTests extends BaseTest {
     }
 
     @Test
-    @Order(7)
+    @Order(8)
     public void deleteNotificationByIdTest_idIsNotPresent() {
         long notificationId = 5321;
-        ResponseEntity<Error> response = template.exchange(createURLWithPort() + "/{notificationId}",
-                HttpMethod.DELETE, null, Error.class, notificationId);
+        ResponseEntity<ProblemDetail> response = template.exchange(createURLWithPort() + "/{notificationId}",
+                HttpMethod.DELETE, null, ProblemDetail.class, notificationId);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
-    public void createNotificationCommentTest_OK() {
+    @Order(9)
+    public void deleteNotificationByIdTest_wrongInput() {
+        String notificationId = "321ffs";
+        ResponseEntity<ProblemDetail> response = template.exchange(createURLWithPort() + "/{notificationId}",
+                HttpMethod.DELETE, null, ProblemDetail.class, notificationId);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+        ProblemDetail details = response.getBody();
+        assertNotNull(details);
+        Map<String, Object> map = details.getProperties();
+        assertNotNull(map);
+
+        ArrayList<?> list = (ArrayList<?>) map.get("problemDetails");
+        LinkedHashMap<?,?> innerData = (LinkedHashMap<?,?>) list.get(0);
+        String actual = innerData.get("wrongValue").toString();
+        assertEquals(notificationId, actual);
+    }
+
+    @Test
+    @Order(10)
+    public void createPostCommentedTest_OK() {
         LocalDateTime dateTime = LocalDateTime.of(2019, 12, 5, 12, 12);
         NotificationContent content = new PostCommented(4L, 3L, "some post...", "some comment");
         NotificationDto getDTO = new NotificationDto(321L, 123L, content, dateTime, null);
@@ -137,7 +175,8 @@ public class IntegrationTests extends BaseTest {
     }
 
     @Test
-    public void createNotificationCommentTest_InvalidUserId() {
+    @Order(11)
+    public void createPostCommentedTest_InvalidUserId() {
         LocalDateTime dateTime = LocalDateTime.of(2019, 12, 5, 12, 12);
         NotificationContent content = new PostCommented(4L, 3L, "some post...", "some comment");
         NotificationDto getDTO = new NotificationDto(-321L, 123L, content, dateTime, null);
@@ -154,5 +193,35 @@ public class IntegrationTests extends BaseTest {
         LinkedHashMap<?,?> innerData = (LinkedHashMap<?,?>) list.get(0);
         String actual = innerData.get("wrongValue").toString();
         assertEquals("-321", actual);
+    }
+
+    @Test
+    @Order(12)
+    public void createCommentRepliedTest_OK() {
+        LocalDateTime dateTime = LocalDateTime.of(2019, 12, 5, 12, 12);
+        NotificationContent content = new CommentReplied(4L, 3L, "some post...", "some comment");
+        NotificationDto getDTO = new NotificationDto(321L, 123L, content, dateTime, null);
+
+        ResponseEntity<NotificationResponseDTO> response = template.postForEntity(createURLWithPort(),
+                getDTO, NotificationResponseDTO.class);
+        NotificationResponseDTO responseDTO = response.getBody();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(responseDTO);
+        assertEquals(COMMENT_REPLIED, responseDTO.getType());
+    }
+
+    @Test
+    @Order(13)
+    public void createUserRegisteredTest_OK() {
+        LocalDateTime dateTime = LocalDateTime.of(2019, 12, 5, 12, 12);
+        NotificationDto getDTO = new NotificationDto(321L, null,
+                new UserRegistered("Oleksii", "oleksiisynelnyk@gmail.com", "some link", 15), dateTime, null);
+
+        ResponseEntity<NotificationResponseDTO> response = template.postForEntity(createURLWithPort(),
+                getDTO, NotificationResponseDTO.class);
+        NotificationResponseDTO responseDTO = response.getBody();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(responseDTO);
+        assertEquals(USER_REGISTERED, responseDTO.getType());
     }
 }
--- src/test/java/com/facedynamics/notifications/services/EmailServiceUnitTest.java
+++ src/test/java/com/facedynamics/notifications/services/EmailServiceUnitTest.java
@@ -2,8 +2,10 @@ package com.facedynamics.notifications.services;
 
 import com.facedynamics.BaseTest;
 import com.facedynamics.notifications.model.NotificationUserServiceDTO;
+import com.facedynamics.notifications.model.dto.CommentReplied;
 import com.facedynamics.notifications.model.dto.NotificationDto;
 import com.facedynamics.notifications.model.dto.PostCommented;
+import com.facedynamics.notifications.model.dto.UserRegistered;
 import lombok.RequiredArgsConstructor;
 import org.apache.velocity.app.VelocityEngine;
 import org.junit.jupiter.api.Assertions;
@@ -36,7 +38,7 @@ public class EmailServiceUnitTest extends BaseTest {
     }
 
     @Test
-    public void sendCommentEmailTest() throws Exception {
+    public void sendPostCommentedTest() throws Exception {
         LocalDateTime dateTime = LocalDateTime.of(2019, 12, 5, 12, 12);
         NotificationDto getDTO = new NotificationDto(321L, 123L,
                 new PostCommented(4L, 3L, "some post...", "some comment"), dateTime, null);
@@ -69,5 +71,70 @@ public class EmailServiceUnitTest extends BaseTest {
         Assertions.assertTrue(context.contains("Dragon has left the comment <i>\"some comment ...\" </i>"));
         Assertions.assertTrue(context.contains("on your post <i>\"some post... ...\" </i><br/>"));
         Assertions.assertTrue(context.contains("at 2019-12-05T12:12"));
+    }
+
+    @Test
+    public void sendCommentRepliedTest() throws Exception {
+        LocalDateTime dateTime = LocalDateTime.of(2019, 12, 5, 12, 12);
+        NotificationDto getDTO = new NotificationDto(321L, 123L,
+                new CommentReplied(4L, 3L, "some comment", "some reply"), dateTime, null);
+        NotificationUserServiceDTO userServiceDTO321 = NotificationUserServiceDTO.builder()
+                .ownerName("Oleksii")
+                .username("Unfabled")
+                .email("alex0destroyer@gmail.com")
+                .build();
+        NotificationUserServiceDTO userServiceDTO123 = NotificationUserServiceDTO.builder()
+                .username("Dragon")
+                .build();
+
+        service.sendEmail(getDTO, userServiceDTO321, userServiceDTO123.getUsername());
+
+        ArgumentCaptor<MimeMessagePreparator> captor = ArgumentCaptor.forClass(MimeMessagePreparator.class);
+        verify(mailSender).send(captor.capture());
+        MimeMessagePreparator pr = captor.getValue();
+        Assertions.assertNotNull(pr);
+
+        Field[] fields = pr.getClass().getDeclaredFields();
+        String context = "";
+        for (Field f: fields) {
+            f.setAccessible(true);
+            Object obj = f.get(pr);
+            if (obj instanceof StringWriter){
+                context = obj.toString();
+            }
+        }
+        Assertions.assertTrue(context.contains("Hello Oleksii, you have a new reply to your comment!"));
+        Assertions.assertTrue(context.contains("Dragon has replied <i>\"some reply ...\" </i>"));
+        Assertions.assertTrue(context.contains("on your comment <i>\"some comment ...\" </i><br/>"));
+        Assertions.assertTrue(context.contains("at 2019-12-05T12:12"));
+    }
+
+    @Test
+    public void sendUserRegisteredTest() throws Exception {
+        LocalDateTime dateTime = LocalDateTime.of(2019, 12, 5, 12, 12);
+        NotificationDto getDTO = new NotificationDto(321L, null,
+                new UserRegistered("Oleksii", "some_mail@gmail.com", "some link", 15), dateTime, null);
+
+        service.sendEmail(getDTO, null, null);
+
+        ArgumentCaptor<MimeMessagePreparator> captor = ArgumentCaptor.forClass(MimeMessagePreparator.class);
+        verify(mailSender).send(captor.capture());
+        MimeMessagePreparator pr = captor.getValue();
+        Assertions.assertNotNull(pr);
+
+        Field[] fields = pr.getClass().getDeclaredFields();
+        String context = "";
+        for (Field f: fields) {
+            f.setAccessible(true);
+            Object obj = f.get(pr);
+            if (obj instanceof StringWriter){
+                context = obj.toString();
+            }
+        }
+        Assertions.assertTrue(context.contains("Hello Oleksii, you just registered your account with the email:"));
+        Assertions.assertTrue(context.contains("some_mail@gmail.com <br/>"));
+        Assertions.assertTrue(context.contains("at 2019-12-05T12:12</h3>"));
+        Assertions.assertTrue(context.contains("One last step, just click here on a <a href=\"some link\">Registration Link</a><br/>"));
+        Assertions.assertTrue(context.contains("Please note, the above link is only valid for 15 minutes."));
     }
 }
