61e2d4994eb1855311370223c07e0c9da843e6e7
Oleksii Synelnyk <osynelnyk@griddynamics.com>
2023-03-24 15:24:17 +0200

    change settings for integration

--- src/main/java/com/facedynamics/notifications/controllers/UserEventService.java
+++ src/main/java/com/facedynamics/notifications/controllers/UserEventService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${user.service.url}")
+@FeignClient(name = "users-service", url = "${user.service.url}")
 @Component
 public interface UserEventService {
     /**
